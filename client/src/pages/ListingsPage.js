ea15ea7307b1fda685424f8fe7c242e4f793a728
zawedcvg <neeladri02@gmail.com>
2021-07-02 12:21:17 +0400

    changes the links and completed listings and report item

--- client/src/pages/ListingsPage.js
+++ client/src/pages/ListingsPage.js
@@ -1,5 +1,145 @@
+import MetaTags from "react-meta-tags";
+import ListingsPageCSS from "../styles/ListingsPage.module.css";
 const ListingsPage = () => {
-    return <h1>Listings boi!</h1>;
+    const Items = [
+        {
+            key: 1,
+            img: "https://source.unsplash.com/random/200x200",
+            date: "20th August 2020",
+            time: "20:00",
+            location: "Near USC",
+            description: "Something, something",
+        },
+    ];
+    return (
+        <div className={ListingsPageCSS.scroll_listings}>
+            <MetaTags>
+                <meta charSet="UTF-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, 
+        initial-scale = 1.0"
+                />
+                <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+                <title>Document</title>
+            </MetaTags>
+            <div className={ListingsPageCSS.container}>
+                <div
+                    className={ListingsPageCSS.top}
+                    id={ListingsPageCSS.something}
+                >
+                    <h1 className={ListingsPageCSS.listings_text}>Listings</h1>
+                    <nav>
+                        <a href="#something">Lost</a>
+                        <a href="#something">Found</a>
+                        <a href="#something">Both</a>
+                    </nav>
+                </div>
+                <div className={ListingsPageCSS.bottom_part}>
+                    <div className={ListingsPageCSS.listings_body}>
+                        <div className={ListingsPageCSS.post}>
+                            <img
+                                src="https://source.unsplash.com/random/200x200"
+                                alt=""
+                            />
+                            <div className={ListingsPageCSS.post_content}>
+                                <span>
+                                    Found on: 20th August, 2021 <br />
+                                </span>
+                                <span>
+                                    Approximate time: 20:00 <br />
+                                </span>
+                                <span>
+                                    Location: Near USC <br />
+                                </span>
+                                <span>
+                                    Description <br />
+                                </span>
+                            </div>
+                        </div>
+                        <div className={ListingsPageCSS.post}>
+                            <img
+                                src="https://source.unsplash.com/random/200x200"
+                                alt=""
+                            />
+                            <div className={ListingsPageCSS.post_content}>
+                                <span>
+                                    Found on: 20th August, 2021 <br />
+                                </span>
+                                <span>
+                                    Approximate time: 20:00 <br />
+                                </span>
+                                <span>
+                                    Location: Near USC <br />
+                                </span>
+                                <span>
+                                    Description <br />
+                                </span>
+                            </div>
+                        </div>
+                        <div className={ListingsPageCSS.post}>
+                            <img
+                                src="https://source.unsplash.com/random/200x200"
+                                alt=""
+                            />
+                            <div className={ListingsPageCSS.post_content}>
+                                <span>
+                                    Found on: 20th August, 2021 <br />
+                                </span>
+                                <span>
+                                    Approximate time: 20:00 <br />
+                                </span>
+                                <span>
+                                    Location: Near USC <br />
+                                </span>
+                                <span>
+                                    Description <br />
+                                </span>
+                            </div>
+                        </div>
+                        {Items.map((items) => (
+                            <Posts
+                                key={items.key}
+                                img={items.img}
+                                date={items.date}
+                                time={items.time}
+                                location={items.location}
+                                description={items.description}
+                            />
+                        ))}
+                    </div>
+                    <form>
+                        <button className={ListingsPageCSS.btn_listings}>
+                            Make a report
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const Posts = ({ img, date, time, location, description }) => {
+    return (
+        <div className={ListingsPageCSS.post}>
+            <img src={img} alt="" />
+            <div className={ListingsPageCSS.post_content}>
+                <span>
+                    Found on: {date} <br />
+                </span>
+                <span>
+                    Approximate time: {time} <br />
+                </span>
+                <span>
+                    Location: {location}
+                    <br />
+                </span>
+                <span>
+                    Description: {description} <br />
+                </span>
+            </div>
+        </div>
+    );
 };
 
 export default ListingsPage;
---
+++ client/src/pages/ReportItem.js
@@ -0,0 +1,54 @@
+import React from "react";
+import MetaTags from "react-meta-tags";
+import ReportItemCSS from "../styles/ReportItemPage.module.css"
+
+function ReportItem() {
+    return (
+        <div className="ReportItem">
+            <MetaTags>
+                <meta charset="UTF-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, 
+        initial-scale = 1.0"
+                />
+                <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+                <title>Report An Item</title>
+            </MetaTags>
+            <div className={ReportItemCSS.container}>
+                <div className={ReportItemCSS.details}>
+                    <h1>Report an Item</h1>
+                    <form>
+                        <label htmlFor="location">
+                            Approximate Location <br />
+                            of the Item :
+                            <input type="text" className={ReportItemCSS.location} />
+                        </label>
+                        <label htmlFor="description">
+                            Description of the object :
+                            <textarea
+                                id="description"
+                                className={ReportItemCSS.textarea}
+                                name="description"
+                            ></textarea>
+                        </label>
+                        <label htmlFor="date-found">
+                            Date of Happening :
+                            <input type="date" className={`${ReportItemCSS.date_found} ${ReportItemCSS.small}`} />
+                        </label>
+                        <label htmlFor="time">
+                            Approximate time :
+                            <input type="time" className="small" />
+                        </label>
+                    </form>
+                    <button className={ReportItemCSS.btn}>Submit</button>
+                    <span>or</span>
+                    <button className={ReportItemCSS.btn}>Go to listings</button>
+                </div>
+                <img src="https://source.unsplash.com/random" alt="something" />
+            </div>
+        </div>
+    );
+}
+
+export default ReportItem;
