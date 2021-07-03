b7166e5048fa90aec3c98df345b382cc87f585f8
s7manth <syalamarty002@gmail.com>
2021-07-03 14:59:01 +0800

    modified server and package.json

--- server/server.js
+++ server/server.js
@@ -40,6 +40,10 @@ mongoose.connect(
     }
 );
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("../client/build"))
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
