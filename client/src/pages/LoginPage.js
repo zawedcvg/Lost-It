e2ec8b07a159dded5190d02916b56ec9aaefbde1
s7manth <syalamarty002@gmail.com>
2021-07-04 17:44:05 +0800

    Please check the dependency tree, issues with package imports

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -5,7 +5,7 @@ import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import { dispatchLogin } from "../redux/actions/authAction";
 import { useDispatch } from "react-redux";
-// import { GoogleLogin } from 'react-google-login';
+import { GoogleLogin } from 'react-google-login';
 // import FacebookLogin from 'react-facebook-login';
 
 const initialState = {
@@ -50,22 +50,22 @@ function LoginPage() {
     //     history.push("/register")
     // }
 
-    // const responseGoogle = async (response) => {
-    //     try {
-    //         const res = await axios.post("/user/google_login", {
-    //             tokenId: response.tokenId,
-    //         });
+    const responseGoogle = async (response) => {
+        try {
+            const res = await axios.post("/user/google_login", {
+                tokenId: response.tokenId,
+            });
 
-    //         setUser({ ...user, error: "", success: res.data.msg });
-    //         localStorage.setItem("firstLogin", true);
+            setUser({ ...user, error: "", success: res.data.msg });
+            localStorage.setItem("firstLogin", true);
 
-    //         dispatch(dispatchLogin());
-    //         history.push("/");
-    //     } catch (err) {
-    //         err.response.data.msg &&
-    //             setUser({ ...user, err: err.response.data.msg, success: "" });
-    //     }
-    // };
+            dispatch(dispatchLogin());
+            history.push("/");
+        } catch (err) {
+            err.response.data.msg &&
+                setUser({ ...user, err: err.response.data.msg, success: "" });
+        }
+    };
 
     // const responseFacebook = async (response) => {
     //     try {
@@ -151,23 +151,24 @@ function LoginPage() {
                     Forgot your password?
                 </Link>
             </div>
+        
+        <div className="social">
+            <GoogleLogin
+                clientId="209721098919-5joiqq888tl1vd0uh0434tolb0jrnrnc.apps.googleusercontent.com"
+                buttonText="Login with Google"
+                onSuccess={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
+
+            {/* <FacebookLogin
+            appId="Your facebook app id"
+            autoLoad={false}
+            fields="name,email,picture"
+            callback={responseFacebook}
+            /> */}
+
         </div>
-        // {/* <div className="social">
-        //         <GoogleLogin
-        //             clientId="Your google client id"
-        //             buttonText="Login with google"
-        //             onSuccess={responseGoogle}
-        //             cookiePolicy={'single_host_origin'}
-        //         />
-
-        //         <FacebookLogin
-        //         appId="Your facebook app id"
-        //         autoLoad={false}
-        //         fields="name,email,picture"
-        //         callback={responseFacebook}
-        //         />
-
-        //     </div> */}
+        </div>
     );
 }
 
