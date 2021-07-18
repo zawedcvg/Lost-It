ceacf96e563f974ae2c2303f4f68eb8dc8b0e5d2
s7manth <syalamarty002@gmail.com>
2021-07-18 21:12:16 +0800

    heroku step

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const fileUpload = require("express-fileupload");
 const userRouter = require("./routes/userRouter.js");
 const upload = require("./routes/upload.js");
 const postRouter = require("./routes/postRouter.js");
-const path = require("path");
+// const path = require("path");
 
 const app = express();
 
@@ -46,14 +46,14 @@ mongoose.connect(
 //     app.use(express.static("../client/build"));
 // }
 
-if (process.env.NODE_ENV === "production") {
-    // Set static folder
-    app.use(express.static("./client/build"));
+// if (process.env.NODE_ENV === "production") {
+//     // Set static folder
+//     app.use(express.static("./client/build"));
 
-    app.use("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
+//     app.use("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
