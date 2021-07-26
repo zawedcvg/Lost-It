04b724942f763eb7dd1c966e71c8be90af9473e2
s7manth <syalamarty002@gmail.com>
2021-07-26 22:33:50 +0800

    tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -18,3 +18,21 @@ test("renders the output of the text display on Main Page", () => {
     const linkElement = screen.getByText(/A digitized lost & found system/);
     expect(linkElement).toBeInTheDocument();
 });
+
+// test("renders the output of the text display : User Dashboard", () => {
+//     render(<App />);
+//     const linkElement = screen.getByText(/User Dashboard/);
+//     expect(linkElement).toBeInTheDocument();
+// });
+
+test("renders the output of the text display : Sign Up", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/A digitized lost & found system/);
+    expect(linkElement).toBeInTheDocument();
+});
+
+test("renders the output of the text display on Listings", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Log In/);
+    expect(linkElement).toBeInTheDocument();
+});
